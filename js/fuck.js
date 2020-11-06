ec9d7f9eef1a6de9d1f7cca11598d43a6a54e64a
Вячеслав Андреев <53828303+maxivimax@users.noreply.github.com>
2020-11-06 19:50:18 +0300

    Add files via upload

---
+++ js/fuck.js
@@ -0,0 +1,11 @@
+function moveBlock(idBlock, idBlock2){
+	var ot = document.getElementById(idBlock);
+	// alert(ot.style.cssText);
+	// 	document.getElementById(idBlock2).style="background-color: white;"
+		// return "It's pixel don't закрашен!"	
+	document.getElementById(idBlock).style="background-color: white;"
+	document.getElementById(idBlock2).style="background-color: red;"
+
+	return "Done!"
+		
+}
